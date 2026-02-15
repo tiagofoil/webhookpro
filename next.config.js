8d7e7d4da25c7e49f5fc5d32060ca0cc6a2aeea2
tiagofoil <tiagofoil@gmail.com>
2026-02-15 03:12:43 +0000

    Remove rewrite rule - use separate URLs for API and UI

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/hook/:id',
-        destination: '/api/webhook/:id',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
