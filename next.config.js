bf896508387190ce4874871cc84e9373cdb38362
tiagofoil <tiagofoil@gmail.com>
2026-02-15 02:38:15 +0000

    Add rewrite: /hook/{id} → /api/webhook/{id}

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/hook/:id',
+        destination: '/api/webhook/:id',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
